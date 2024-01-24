d43ab268d58853dd6e83f3403b40620e62033857
Lucão <lukaspc2019@gmail.com>
2024-01-24 12:57:48 -0300

    initial commit

---
+++ variaveis.py
@@ -0,0 +1,14 @@
+""" Variáveis -> Usamos para definir dados simples
+
+    Regra 1 - Não pode declarar variável com carácter especial/números (sem acentos)
+        Exemplo certo:
+                        nome_completo
+
+    -> Python existe tipagem dinâmica, ou seja, se eu definir (nome_completo = "Lucas Carvalho") o python já sabe que é do tipo string
+
+    Camel Case -> Utilizado para classes
+    Snake Case -> Utilizado para variáveis/funções e métodos
+
+"""
+
+nome_completo = "Lucas Carvalho"
